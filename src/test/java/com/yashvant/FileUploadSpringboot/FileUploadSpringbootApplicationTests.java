b71f18516dc8e0532fccf332a8a79ed605fa14c9
yashvant <yashvantyadav855@gmail.com>
2023-10-24 23:14:25 +0530

    Initial commit

---
+++ src/test/java/com/yashvant/FileUploadSpringboot/FileUploadSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashvant.FileUploadSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
